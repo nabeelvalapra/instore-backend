5d9c9e8f81ba2487cb1f7eb6aba89bf3e6836dc0
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-04-18 20:15:55 +0530

    Made basic structure

---
+++ frontend/src/app/home/HomeComponent.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function HomeComponent() {
+  return (
+    <div>
+      Hello Hello
+    </div>
+  )
+}
+
+export default HomeComponent;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+import App from './app';
+
+const store = createStore(rootReducer)
+
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
