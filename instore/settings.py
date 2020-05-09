9561fc68242bd1f3c3131e0b4de76762caf40a34
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-09 11:14:45 +0530

    Heroku fixes

--- instore/settings.py
+++ instore/settings.py
@@ -56,6 +56,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'instore.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'instore.wsgi.application'
 
 
@@ -112,6 +128,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
