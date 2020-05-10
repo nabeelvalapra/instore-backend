f7b105b05efe10bf9243e6397f7bc3258daecda8
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-10 22:56:57 +0530

    Improved middleware

--- backend/base/middleware.py
+++ backend/base/middleware.py
@@ -1,4 +1,5 @@
 from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import SuspiciousOperation
 from django.contrib.sites.models import Site
@@ -11,6 +12,10 @@ class CurrentSiteMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         header_origin = request.headers.get('Origin', None)
-        if not header_origin:
+        request_uri = request.get_raw_uri()
+        if all(key in request_uri for key in ['/admin/', settings.REQUEST_SCHEMA]):
+            request.site = get_object_or_404(Site, id=1)
+        elif not header_origin:
             raise SuspiciousOperation("Missing Origin Header in Request")
-        request.site = get_object_or_404(Site, domain__exact=header_origin)
+        else:
+            request.site = get_object_or_404(Site, domain__exact=header_origin)
