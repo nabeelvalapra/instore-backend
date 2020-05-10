6d8c9e25a4d7f1e5c2df031156218dab8ae59563
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-10 22:33:55 +0530

    moved up cors middleware

--- instore/settings/base.py
+++ instore/settings/base.py
@@ -46,10 +46,10 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'instore_user.InstoreUser'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'base.middleware.CurrentSiteMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
