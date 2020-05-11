7d1834179a077cee34ef29f03d0db81362170d1b
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-11 15:52:41 +0530

    amend

--- fabfile.py
+++ fabfile.py
@@ -9,3 +9,4 @@ def deploy_backend(c):
     c.run("cd instore && git reset --hard origin/master")
     c.run("cd instore && git pull origin master")
     c.run("cd instore && /home/admin/venv/instore/bin/python3 manage.py migrate")
+    c.run("sudo service supervisor restart && sudo service nginx restart", pty=True)
