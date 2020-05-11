22bbd1aa8fae93be631547f8dae2961a841085bf
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-11 16:28:29 +0530

    Added fabfile

--- fabfile.py
+++ fabfile.py
@@ -10,3 +10,8 @@ def deploy_backend(c):
     c.run("cd instore && git pull origin master")
     c.run("cd instore && /home/admin/venv/instore/bin/python3 manage.py migrate")
     c.run("sudo service supervisor restart && sudo service nginx restart", pty=True)
+
+
+@task(hosts=backend_host)
+def deploy_frontend(c):
+    c.run("cd instore/frontend/ && npm run build")
