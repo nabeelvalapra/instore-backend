dc81bce6adb51c484126d243deb8f6d282a9771b
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-13 14:46:02 +0530

    improvement in the setting file

--- backend/base/middleware.py
+++ backend/base/middleware.py
@@ -15,17 +15,16 @@ class CurrentSiteMiddleware(MiddlewareMixin):
         header_origin = request.headers.get('Origin', None)
         request_uri = request.get_raw_uri()
         if not header_origin:
-            if '/admin/' in request_uri and settings.REQUEST_SCHEME in request_uri:
+            if '/admin/' in request_uri and request.is_secure():
                 request.store = get_object_or_404(
                     Store, domain=settings.SUPERUSER_STORE_DOMAIN
                 )
+            elif settings.DEBUG:
+                request.store = get_object_or_404(Store, id=settings.DEBUG_STORE_ID)
             else:
-                if settings.get("DEBUG_STORE_ID", None):
-                    request.store = get_object_or_404(Store, id=settings.DEBUG_STORE_ID)
-                else:
-                    raise SuspiciousOperation(
-                        "Missing Origin Header in Request - {}".format(request_uri)
-                    )
+                raise SuspiciousOperation(
+                    "Missing Origin Header in Request - {}".format(request_uri)
+                )
         else:
-            header_origin = header_origin.replace(settings.REQUEST_SCHEME, "")
+            header_origin = header_origin.replace(request.scheme + "://", "")
             request.store = get_object_or_404(Store, domain__exact=header_origin)
