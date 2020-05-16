f996bebdfbdb281701d363fda59b0e42d8029782
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-16 14:43:18 +0530

    updated fab file

--- fabfile.py
+++ fabfile.py
@@ -6,16 +6,14 @@ backend_host = ["instore-backend"]
 
 @task(hosts=backend_host)
 def deploy_backend(c):
-    c.run("cd instore && git reset --hard origin/master")
-    c.run("cd instore && git pull origin master")
-    c.run("cd instore && /home/admin/venv/instore/bin/python3 manage.py migrate")
+    c.run("cd instore-backend && git reset --hard origin/master")
+    c.run("cd instore-backend && git pull origin master")
+    c.run("cd instore-backend && /home/admin/venv/instore/bin/python3 manage.py migrate")
     c.run("sudo service supervisor restart && sudo service nginx restart", pty=True)
 
 
 @task(hosts=backend_host)
 def deploy_frontend(c):
-    c.run("cd instore && git reset --hard origin/master")
-    c.run("cd instore && git pull origin master")
-    c.run("cd instore/frontend/ && npm install && npm run build")
-    c.run("rm -r /home/admin/frontend-build && "
-          "mv /home/admin/instore/frontend/build /home/admin/frontend-build")
+    c.run("cd instore-frontend && git reset --hard origin/master")
+    c.run("cd instore-frontend && git pull origin master")
+    c.run("cd instore-frontend && npm install && npm run build")
